f41b464ba48a0a46a45fdde6ab76ebf1d6358099
Ana Beatriz Coelho <bia.e@live.com>
2024-06-28 15:33:31 -0300

    alterei taskmodel

--- src/main/java/br/com/beatrizcoelho/todolist/task/TaskModel.java
+++ src/main/java/br/com/beatrizcoelho/todolist/task/TaskModel.java
@@ -31,7 +31,7 @@ public class TaskModel {
 
     @Column(length = 50)
     private String title;
-    private LocalDateTime startAt; //consegue colocar data e horario
+    private LocalDateTime startAt; 
     private LocalDateTime endAt;
     private String priority;
 
